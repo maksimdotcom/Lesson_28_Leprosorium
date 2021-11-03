5a129bde383ef8460a905014c5b5f30a984bb9a1
maksimdotcom <ya.maksim0511@gmail.com>
2021-11-03 19:25:22 +0300

    add comments

--- app.rb
+++ app.rb
@@ -9,12 +9,20 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы
+
 before do
+	# инициализация базы данных
 	init_db
 end
 
-configure do
-	init_db 
+# configure  вызывается каждый раз при конфигурации приложения
+# когда изменился код и перезагрузилась страница
+configure do 
+	# инициализация базы данных
+	init_db
+	# создает таблицу если не существет
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -27,11 +35,16 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new
+#(браузер получает страницу с сервера)
 get '/new' do
 	erb :new
 end
 
+# обраблтчик post-запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+	# получаем переменную из пост-запроса
 	content = params[:content]
 
 	erb "You typed #{content}"
